129596ff1ce934ea068c59a7d4bbde3fbc9d9c35
Yan He <947391619@qq.com>
2021-10-30 16:20:37 -0500

    mybatis connect

--- soccerGod/src/main/java/com/fouroneone/soccergod/SoccerGodApplication.java
+++ soccerGod/src/main/java/com/fouroneone/soccergod/SoccerGodApplication.java
@@ -3,7 +3,7 @@ package com.fouroneone.soccergod;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
+//如果需要多数据源 这个配置存在，当配置好了application.yml中的数据源后，就不用这句exclude的注解得了
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class SoccerGodApplication {
 
