e778a1ddca68279f4a8d57041404e27de281b3fb
MasenWang <zhewang8@illinois.edu>
2021-11-07 21:02:34 -0600

    feat(team): team名称搜索

--- soccerGod/src/main/java/com/fouroneone/soccergod/controller/SearchController.java
+++ soccerGod/src/main/java/com/fouroneone/soccergod/controller/SearchController.java
@@ -3,31 +3,39 @@ package com.fouroneone.soccergod.controller;
 import com.fouroneone.soccergod.bean.Team;
 import com.fouroneone.soccergod.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RestController
-@RequestMapping("/search")
+//@RestController
+//@RequestMapping("/search")
+@Controller
 public class SearchController {
     @Autowired
     TeamService teamService;
 
-    @RequestMapping("/teamkeyword")
-    @CrossOrigin(origins = "http://localhost:3000")
-    public Map<String, List<Team>> getTeamByKeyword(@RequestBody Map<String, Object> payLoad) {
-        String keyword = (String) payLoad.get("keyword");
+    @RequestMapping(method = RequestMethod.POST, value="/teamkeyword")
+    public String getTeamByKeyword(@RequestParam("keyword") String keyword, Model model) {
         List<Team> nameKey = teamService.getTeamByName(keyword);
-        List<Team> descKey = teamService.getTeamByDesc(keyword);
-        Map<String, List<Team>> mp = new HashMap<>();
-        mp.put("names", nameKey);
-        mp.put("descs", descKey);
-        return mp;
+        model.addAttribute("teams", nameKey);
+
+        return "search";
     }
+
+//    @RequestMapping("/teamkeyword")
+//    @CrossOrigin(origins = "http://localhost:3000")
+//    public Map<String, List<Team>> getTeamByKeyword(@RequestBody Map<String, Object> payLoad) {
+//        String keyword = (String) payLoad.get("keyword");
+//        List<Team> nameKey = teamService.getTeamByName(keyword);
+//        List<Team> descKey = teamService.getTeamByDesc(keyword);
+//        Map<String, List<Team>> mp = new HashMap<>();
+//        mp.put("names", nameKey);
+//        mp.put("descs", descKey);
+//        return mp;
+//    }
 }
