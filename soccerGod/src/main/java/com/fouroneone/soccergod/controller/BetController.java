90878c2a8db6e834a048d8c9a32d99876c88fd02
MasenWang <zhewang8@illinois.edu>
2021-12-01 01:28:22 -0600

    fix(bet): 下注补充非空判断

--- soccerGod/src/main/java/com/fouroneone/soccergod/controller/BetController.java
+++ soccerGod/src/main/java/com/fouroneone/soccergod/controller/BetController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 @Controller
 @CrossOrigin(origins = "http://localhost:3000")
@@ -37,30 +38,40 @@ public class BetController {
             result = new Result("Could not finish bet as user not login", false, null);
         } else {
             System.out.println(request.getParameter("points"));
-            int betPoints = Integer.parseInt(request.getParameter("points"));
+            String pointsstr = request.getParameter("points");
+            if (Objects.equals(pointsstr, "")) {
+                result = new Result("Could not finish bet because of null value", false, null);
+                return result;
+            }
+            Integer betPoints = Integer.parseInt(pointsstr);
             int mid = Integer.parseInt(request.getParameter("mid"));
             String username = (String)request.getSession().getAttribute("username");
             int hostWin = Integer.parseInt(request.getParameter("hostWin"));
             int points = userService.getPoints(username);
             int ponds = competitionService.findPondAmountById(mid);
+            if (betPoints<=0) {
+                result = new Result("Could not finish bet because of negative or null bet points", false, null);
+                return result;
+            }
             if (points-betPoints<0) {
                 result = new Result("Could not finish bet because of insufficient balance", false, null);
-            } else {
-                userService.updatePoints(username, points - betPoints);
-                competitionService.UpdatePondAmount(mid, ponds + betPoints);
-                Guess guess = new Guess();
-                guess.setAmount(betPoints);
-                guess.setMid(mid);
-                guess.setUsername(username);
-                Date date=new Date();//此时date为当前的时间
-                SimpleDateFormat dateFormat=new SimpleDateFormat("YYYY-MM-dd");
-                String date_str = dateFormat.format(date);
-                guess.setTime(date_str);
-                guess.setHostwin(hostWin);
-                guess.setWin(-1);
-                guessService.addNewGuess(guess);
-                result = new Result("Bet succ", true, null);
+                return result;
             }
+            userService.updatePoints(username, points - betPoints);
+            competitionService.UpdatePondAmount(mid, ponds + betPoints);
+            Guess guess = new Guess();
+            guess.setAmount(betPoints);
+            guess.setMid(mid);
+            guess.setUsername(username);
+            Date date=new Date();//此时date为当前的时间
+            SimpleDateFormat dateFormat=new SimpleDateFormat("YYYY-MM-dd");
+            String date_str = dateFormat.format(date);
+            guess.setTime(date_str);
+            guess.setHostwin(hostWin);
+            guess.setWin(-1);
+            guessService.addNewGuess(guess);
+            result = new Result("Bet succ", true, null);
+
         }
         return result;
     }
